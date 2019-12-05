711861e111e9e0eb00bb7771c7ec122b524d5ec8
Joel  Bly <jtb6179@g.rit.edu>
2019-12-05 18:08:49 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -6,11 +6,11 @@ def find_item_by_name_in_collection(name, collection)
   # Consult README for inputs and outputs
   
   bg = 0 
-  while bg < collection.length do
-  collection[bg] 
+  while bg < collection.length do 
+    collection[bg] 
   #binding.pry
     if name == collection[bg][:item]
-      
+      collection[bg]
     end 
     bg += 1 
   end
