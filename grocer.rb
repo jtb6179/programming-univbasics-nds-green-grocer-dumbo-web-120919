a5eb3e6f1b2a815b210008e05a2a88184d63361f
Joel  Bly <jtb6179@g.rit.edu>
2019-12-05 17:58:22 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -11,7 +11,7 @@ def find_item_by_name_in_collection(name, collection)
     if name == collection[bg][:item]
       collection[bg]
     else 
-      nil  
+      collection[bg]
     end 
     bg += 1 
   end
