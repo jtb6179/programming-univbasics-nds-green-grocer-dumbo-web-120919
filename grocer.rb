91f47ae72552502d868f07f48f4adce7518ced26
Joel  Bly <jtb6179@g.rit.edu>
2019-12-05 18:33:56 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -26,7 +26,7 @@ def consolidate_cart(cart)
   box1= []
   while hg < cart.length do 
     jack_in = find_item_by_name_in_collection(cart[hg][:item], box1)
-    if jack_in
+    if jack_in != nil 
     hg += 1 
   end
   return box 
